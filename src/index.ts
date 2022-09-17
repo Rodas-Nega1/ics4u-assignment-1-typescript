9473ad4de2e3da900927f88a4dd96ad349a7ad8f
Rodas-Nega1 <Rodas-Nega1@users.noreply.github.com>
2022-09-17 20:03:34 +0000

    Prettified Code!

--- src/index.ts
+++ src/index.ts
@@ -7,44 +7,44 @@
  * Since:   2022-09-17
  */
 
-import promptSync from 'prompt-sync'
-import random from 'random'
+import promptSync from "prompt-sync";
+import random from "random";
 
 // The program will halt with a response at improper user inputs
-const prompt = promptSync()
+const prompt = promptSync();
 
-const randomNumber = random.int(1, 3)
+const randomNumber = random.int(1, 3);
 // input
-const player = prompt('Decide on a move (rock, paper, or scissors): ')
-let computer = String(randomNumber)
+const player = prompt("Decide on a move (rock, paper, or scissors): ");
+let computer = String(randomNumber);
 // process & output
 if (randomNumber === 1) {
-  computer = 'rock'
+  computer = "rock";
 } else if (randomNumber === 2) {
-  computer = 'paper'
+  computer = "paper";
 } else if (randomNumber === 3) {
-  computer = 'scissors'
+  computer = "scissors";
 }
 
 if (player === computer) {
-  console.log(`\nThe computer chose ${computer}.`)
-  console.log('\nTie.')
+  console.log(`\nThe computer chose ${computer}.`);
+  console.log("\nTie.");
 } else if (
-  (player === 'rock' && computer === 'paper') ||
-  (player === 'paper' && computer === 'scissors') ||
-  (player === 'scissors' && computer === 'rock')
+  (player === "rock" && computer === "paper") ||
+  (player === "paper" && computer === "scissors") ||
+  (player === "scissors" && computer === "rock")
 ) {
-  console.log(`\nThe computer chose ${computer}.`)
-  console.log('\nYou lost.')
+  console.log(`\nThe computer chose ${computer}.`);
+  console.log("\nYou lost.");
 } else if (
-  (player === 'paper' && computer === 'rock') ||
-  (player === 'scissors' && computer === 'paper') ||
-  (player === 'rock' && computer === 'scissors')
+  (player === "paper" && computer === "rock") ||
+  (player === "scissors" && computer === "paper") ||
+  (player === "rock" && computer === "scissors")
 ) {
-  console.log(`\nThe computer chose ${computer}.`)
-  console.log('\nYou win!')
+  console.log(`\nThe computer chose ${computer}.`);
+  console.log("\nYou win!");
 } else {
-  console.log('\nSomething is not right.')
+  console.log("\nSomething is not right.");
 }
 
-console.log('\nDone.')
+console.log("\nDone.");
